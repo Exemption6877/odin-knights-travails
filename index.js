4dbdeb0b5c01193457f7442940d9a9750fb669c0
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-03 19:01:49 +0300

    Finalize the project

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const knightAction = (function () {
   const moveValidity = (currentPos, nextMove) => {
     const [currentX, currentY] = currentPos;
     const [nextX, nextY] = nextMove;
-    // checking whether both inputs within chessboard
 
     if (currentX > 7 || currentY > 7 || currentX < 0 || currentY < 0) {
       return false;
@@ -48,21 +47,14 @@ const knightAction = (function () {
   };
 
   const generateMove = (currentPos) => {
-    // In general, knight can have min 2 moves, and max 8 moves variations.
-    // I will generate all 8, and then check them with moveValidity()
     const allPossibilities = [];
     const [currentX, currentY] = currentPos;
-    // const queue = [] -- possibly for another function
-    // x+2 y+1 and y-1
     allPossibilities.push([currentX + 2, currentY + 1]);
     allPossibilities.push([currentX + 2, currentY - 1]);
-    // x-2 y+1 and y-1
     allPossibilities.push([currentX - 2, currentY + 1]);
     allPossibilities.push([currentX - 2, currentY - 1]);
-    // y+2 x+1 and x-1
     allPossibilities.push([currentX + 1, currentY + 2]);
     allPossibilities.push([currentX - 1, currentY + 2]);
-    // y-2 x+1 and x-1
     allPossibilities.push([currentX + 1, currentY - 2]);
     allPossibilities.push([currentX - 1, currentY - 2]);
 
@@ -77,42 +69,3 @@ function knightMoves(currentPos, nextMove) {
 }
 
 knightMoves([0, 0], [1, 5]);
-
-/*
-knight's legal move L-shaped
-taking he's at x,y [3,3].
-Max 8 legal moves: group by X
-[1,2],[1,4],
-[2,1],[2,5],
-[4,1],[4,5],
-[5,2],[5,4]
-
--+2xy -+yx
-i can group them up by two: going two times direction then left or right
-*/
-
-/*
-[]
-
-   0   1   2   3   4   5   6   7
-7 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-6 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-5 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-4 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-3 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-2 [ ] [0] [ ] [ ] [ ] [ ] [ ] [ ]
-1 [ ] [ ] [0] [ ] [ ] [ ] [ ] [ ]
-0 [X] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-
-   0   1   2   3   4   5   6   7
-7 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-6 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-5 [ ] [ ] [O] [ ] [O] [ ] [ ] [ ]
-4 [ ] [O] [ ] [ ] [ ] [O] [ ] [ ]
-3 [ ] [ ] [ ] [X] [ ] [ ] [ ] [ ]
-2 [ ] [O] [ ] [ ] [ ] [O] [ ] [ ]
-1 [ ] [ ] [O] [ ] [O] [ ] [ ] [ ]
-0 [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
-
-
-*/
